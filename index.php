cb722e6333d2318e022eafdd2748e1b15fee980e
cristianPari <76077373@lasalleurubamba.edu.pe>
2024-11-29 08:24:50 -0500

    submit
    restaurante

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
     <div class="container">
         <h1>RESTAURANTE:</h1>
         <img src="https://images.rappi.pe/restaurants_logo/a5cb83bc-fe90-421e-b7e2-9a020606bcdb-1618923957931.jpeg" ><br>
-        <a href="clientes/index.php" class="button" style="position: relative; left: 35%;">Lista de clientes</a>
+        <a href="clientes/index.php" class="button" style="position: relative; left: 20%;">Lista de clientes</a>
+        <a href="productos/index.php" class="button" style="position: relative; left: 25%;">Lista de pedidos</a>
     </div>
 </body>
 
